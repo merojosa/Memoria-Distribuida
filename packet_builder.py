50ff688922de88ce3bb2dc2bb5ecdad0b1341bb7
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-09-10 22:25:11 -0600

    Evitar repetición de números de sequencia consecutivos.

--- packet_builder.py
+++ packet_builder.py
@@ -6,10 +6,13 @@ import time
 
 # unsigned char, string of 3 bytes, unsigned char, string of 3 bytes, unsigned char, float.
 FORMAT = 'BIB3sBf'
+last_sequence = 0
 
 # Pending: dealing with date.
 # Create a packet acording to the parameters, is a struct with a specific format (FORMAT)
 def create(team_id, sensor_id, sensor_type, data):
+
+    global last_sequence
 
     # Get current date.
     current_date = datetime.datetime.now()
@@ -17,5 +20,11 @@ def create(team_id, sensor_id, sensor_type, data):
     # Convert the current date to timestamp.  For getting the datetime again: datetime.datetime.fromtimestamp(timestamp)
     timestamp = int(time.mktime(current_date.timetuple()))
 
-    return struct.pack(FORMAT, random.randint(0, 200), timestamp, team_id, sensor_id, sensor_type, data)
+    # Not to repeat a consecutive random number.
+    new_sequence = random.randint(0, 200)
+    while (new_sequence == last_sequence):
+        new_sequence = random.randint(0, 200)
+    last_sequence = new_sequence
+
+    return struct.pack(FORMAT, new_sequence, timestamp, team_id, sensor_id, sensor_type, data)
     pass
