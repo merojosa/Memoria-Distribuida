b842a7b17158d6439df4c66bd16d2c53b7e63ad6
Bryan Ulate <bryan.ulate@ucr.ac.cr>
2019-09-17 15:16:13 -0600

    Arreglado error de envío de datos.  Por Jose Andrés y Josué.

--- dht11_manager.py
+++ dht11_manager.py
@@ -1,26 +1,29 @@
+from datetime import *
 import dht11
 import RPi.GPIO as GPIO
-import time
-import datetime
 
-def get_data():
+def get_data(timeout):
 
     PIN = 17
 
-    GPIO.setwarnings(True)
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
     # Read data
     instance = dht11.DHT11(PIN)
+    
+    temp = 0.0
+    hum = 0.0
+    
+    t1 = datetime.now()
+    
+    while (datetime.now() - t1).seconds <= timeout:
 
-
-    result = instance.read()
-    if result.is_valid():
-        return result.temperature, result.humidity
-    else:
-        print("Error del dht11 sensor: 1")
-        return -1.0, -1.0
-
-
-
-
+    
+        result = instance.read()
+        if result.is_valid():
+            current_date = datetime.now()
+            temp = result.temperature
+            hum = result.humidity
+            
+    return temp, hum, current_date
