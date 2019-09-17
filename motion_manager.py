9ef5375988e272399c0679c735798a90d7305988
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-09-17 08:41:23 -0600

    Mejoras en la iteración, falta testeo en el Raspberry

--- motion_manager.py
+++ motion_manager.py
@@ -2,10 +2,8 @@ import time
 import RPi.GPIO as GPIO
 from gpiozero import MotionSensor
 
-def get_data():
+def get_data(seconds_timeout):
     PIN = 4
-
-    timeout = 0.5   # 0.5 second
 
     timeout_start = time.time()
 
@@ -13,8 +11,8 @@ def get_data():
     pir = MotionSensor(PIN)
 
     # While with a timeout
-    while time.time() < timeout_start + timeout:
-
+    while time.time() < timeout_start + seconds_timeout:
+        time.sleep(0.01) # Not to overwhelm.
         if pir.motion_detected:
             return 1.0
 
