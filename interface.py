55bb3c068b78a729cd1bc99acb8f76348f2bca47
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-10-21 15:13:55 -0600

    Recibir y guardar datos funcional por Sergio Fernández

--- interface.py
+++ interface.py
@@ -12,20 +12,27 @@ def malloc(sensor_id,team_id):
     process_table[(sensor_id,team_id)].last_page = new_page
     process_table[(sensor_id,team_id)].page_list.append(new_page)
 
+def save_data(sensor_id, team_id, date, data):
+    if (sensor_id,team_id) not in process_table.keys():
+        malloc(sensor_id, team_id)
+
+    savedData = "%i{" + str(date) + "} "
+    savedData += "%f{" + str(data) + "}"
+    write(sensor_id, team_id, savedData)
 
 def write(sensor_id, team_id, data: str):
     offset = process_table[(sensor_id,team_id)].last_byte
     page_id = process_table[(sensor_id,team_id)].last_page
-
+    
     if( offset + len(data.encode("utf8")) >= memory_manager.PAGE_SIZE):
         page_id = memory_manager.create_page()
-        #offset = 0
+        offset = 0
         process_table[(sensor_id,team_id)].last_page = page_id
         process_table[(sensor_id,team_id)].page_list.append(page_id)
-        #process_table[(sensor_id,team_id)].last_byte = 0   
+        process_table[(sensor_id,team_id)].last_byte = 0   
     
     memory_manager.write(page_id, data)
-    #process_table[(sensor_id,team_id)].last_byte += len(data.encode("utf8"))
+    process_table[(sensor_id,team_id)].last_byte += len(data.encode("utf8"))
 
 
 def read(sensor_id, team_id):
@@ -36,22 +43,39 @@ def read(sensor_id, team_id):
 #Work in progress, needs case when is picking empty space
 def interpret_data(sensor_id, team_id, page_data_list):
     data_list = []
+    isInteger = True
     for page in page_data_list:
         sensor_data = ""
         data_iter = 0
         for data in page:
             sensor_data += data
             if data_iter >= process_table[(sensor_id,team_id)].data_size:
+
+                if(isInteger):
+                    stringToAppend = '%i' + sensor_data
+                else:
+                    stringToAppend = '%f' + sensor_data
+
+                isInteger = not isinteger
                 data_list.append(sensor_data)
                 sensor_data = ""
                 data_iter = 0
+
     return data_list
   
 
 class ProcessInfo():
     def __init__(self, *args, **kwargs):
         self.last_page = None
-        #self.last_byte = 0
+        self.last_byte = 0
         self.page_list = []
         self.data_size = 0
 
+sensor = 1
+team = 1
+date = 4254
+data = 1424
+#pList = [[123],[12],[311],[123]]
+save_data(sensor,team, date,data)
+#interpret_data(sensor, team, pList)
+print(memory_manager.get_pages([process_table[(sensor,team)].last_page]))
