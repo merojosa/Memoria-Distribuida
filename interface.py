933c33734fd5e5797ccaa3dca1e226eb9a210b6f
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-10-21 15:43:21 -0600

    Guarda los datos que recibe como pares de fecha y datos por Sergio Fernandez

--- interface.py
+++ interface.py
@@ -6,7 +6,6 @@ def malloc(sensor_id,team_id):
     global process_table
     new_page = memory_manager.create_page()
 
-    #if not process_table[(sensor_id,team_id)]:
     process_table[(sensor_id,team_id)] = ProcessInfo()
 
     process_table[(sensor_id,team_id)].last_page = new_page
@@ -39,27 +38,21 @@ def read(sensor_id, team_id):
     page_list = process_table[(sensor_id,team_id)].page_list
     return interpret_data(sensor_id, team_id, memory_manager.get_pages(page_list))
     
-#Work in progress, needs case when is picking empty space
 def interpret_data(sensor_id, team_id, page_data_list):
     data_list = []
     isInteger = True
     for page in page_data_list:
-        #sensor_data = ""
-        #data_iter = 0
         for data in page:
             sensor_data = data
-            #if data_iter >= process_table[(sensor_id,team_id)].data_size:
 
+            
             if(isInteger):
-                stringToAppend = '%i' + sensor_data
+                dataPair = [sensor_data]
+                data_list.append(dataPair)
                 isInteger = False
             else:
-                stringToAppend = '%f' + sensor_data
+                data_list[-1].append(sensor_data)
                 isInteger = True
-
-            data_list.append(stringToAppend)
-                #sensor_data = ""
-                #data_iter = 0
 
     return data_list
   
@@ -71,24 +64,5 @@ class ProcessInfo():
         self.page_list = []
         self.data_size = 8
 
-sensor = 1
-team = 1
-date = 4254
-data = 1424
-date1 = 42543
-data1 = 14243
-date2 = 42544
-data2 = 14244
-date3 = 42545
-data3 = 14245
-date4 = 42546
-data4 = 14246
-#pList = [[123],[12],[311],[123]]
-save_data(sensor,team, date,data)
-save_data(sensor,team, date1,data1)
-save_data(sensor,team, date2,data2)
-save_data(sensor,team, date3,data3)
-save_data(sensor,team, date4,data4)
-#interpret_data(sensor, team, pList)
-print(read(sensor, team))
+
 
