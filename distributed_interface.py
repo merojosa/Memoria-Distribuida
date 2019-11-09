dc89f63d81795d7f76a4aef9a9e2d29d4eb95f8a
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-11-08 22:47:45 -0600

    Primer acercamiento de interfaz distribuida
    Dos hilos:
    
    1. El que envía el paquete al nodo correspondiente. El criterio fue el que tenga más espacio.
    
    2. El que se queda esperando a recibir la respuesta. El hilo que envía el paquete pone en una cola la ip usada, así, este hilo sabrá a quién escuchar.

---
+++ distributed_interface.py
@@ -0,0 +1,60 @@
+import threading
+import queue
+import socket
+import time
+
+NODES_PORT = 5000
+
+page_location = {}
+current_size_nodes = {}
+
+
+def save_page_node(local_queue_packets, ip_node_queue):
+
+    socket_node = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        packet = local_queue_packets.get()
+        # Pending process packet acording the protocol
+        node_ip = choose_node()
+        ip_node_queue.put(node_ip)
+
+        # send message with socket_node...
+
+
+def receive_packet_node(ip_node_queue):
+    while True:
+        ip_node = ip_node_queue.get()
+        # receive size
+    pass
+
+
+def choose_node():
+    biggest_size = -1
+    big_ip = None
+    
+    for node_id in current_size_nodes:
+
+        if(current_size_nodes[node_id] > biggest_size):
+            biggest_size = current_size_nodes[node_id]
+            big_ip = node_id
+
+    return big_ip
+
+
+def main():
+
+    current_size_nodes["127.0.0.1"] = 100
+    
+    local_queue_packets = queue.Queue()
+    ip_node_queue = queue.Queue()
+
+    save_page_node_thread = threading.Thread(target=save_page_node, args=(local_queue_packets, ip_node_queue,))
+    receive_size_node_thread = threading.Thread(target=receive_packet_node, args=(ip_node_queue,))
+
+    save_page_node_thread.start()
+    receive_size_node_thread.start()
+
+    save_page_node_thread.join()
+    receive_size_node_thread.join()
+
+main()
