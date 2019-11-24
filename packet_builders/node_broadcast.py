3df65f8796cae5de6740ec25c57e820438c87435
Sergio Fernandez <sergiofv55@gmail.com>
2019-11-24 02:23:25 -0600

    cambios en escuchar y broadcast de nodo

--- packet_builders/node_broadcast.py
+++ packet_builders/node_broadcast.py
@@ -1,11 +1,9 @@
 import struct
 
-
-# unsigned char, string of 3 bytes, unsigned char, 3 bytes, unsigned char, float.
-FORMAT = 'BB'
+# unsigned char, unsigned int.
+FORMAT = 'BI'
 sequence = 0
 
-# Pending: dealing with date.
 # Create a packet acording to the parameters, is a struct with a specific format (FORMAT)
 def create(operation_id, size):
     return struct.pack(FORMAT, operation_id, size)
