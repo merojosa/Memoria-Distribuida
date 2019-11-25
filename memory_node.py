c44d87b423013609090a9d7db39732e1ed7e805d
Sergio Fernandez <sergiofv55@gmail.com>
2019-11-25 02:18:46 -0600

    file system guarda en tabla de bytes y archivo

--- memory_node.py
+++ memory_node.py
@@ -9,15 +9,16 @@ import socket
 import packet_builders.local_distributed_packet_builder as local_packet_builder
 import packet_builders.node_broadcast as node_broadcast
 import packet_builders.distributed_node_packet_builder as distributed_node_packet_builder
+import packet_builders.node_data_packet_builder as node_data_packet_builder
 from enum_operation_code import Operation_Code
 
 active_interface_ip = ''
 node_id = 0
-max_size = 3000000
+max_size = 100
 size_left = 0
 metadata_pos = 0
-data_pos = max_size
-byte_table = []
+data_pos = max_size-1
+byte_table = bytearray(max_size)#" "*max_size
 page_list = []
 count_node = 0
 
@@ -51,29 +52,50 @@ def add_to_table(op_id, page_id, page_size, data, creation_date, modification_da
     global metadata_pos
     global data_pos
     global byte_table
-    byte_table[metadata_pos] = op_id
-    byte_table[metadata_pos+1] = page_id
-    byte_table[metadata_pos+2] = page_size
-    byte_table[metadata_pos+5] = creation_date
-    byte_table[metadata_pos+8] = modification_date
-    byte_table[metadata_pos+9] = data_pos
-    metadata_pos += 3
-    for aByte in data:
+
+    # Convert the current date to timestamp.  For getting the datetime again: datetime.datetime.fromtimestamp(timestamp)
+    #timestamp = int(time.mktime(current_date.timetuple()))
+    crea_time_bytes = int(time.mktime(creation_date.timetuple()))
+    mod_date_bytes = int(time.mktime(modification_date.timetuple()))
+    
+    bytes_data = node_data_packet_builder.create(op_id, page_id, page_size, crea_time_bytes, mod_date_bytes, data_pos)
+    print(bytes_data)
+    allThis = struct.unpack(node_data_packet_builder.FORMAT, bytes_data)
+    print(allThis) 
+    for meta_byte in bytes_data:
+        byte_table[metadata_pos] = meta_byte
+        metadata_pos += 1
+    data_bytes = bytearray(data, 'utf-8')
+    for aByte in data_bytes:
         byte_table[data_pos] = aByte
         data_pos -= 1
-    metadata_pos += 10
+    print(byte_table)
     wrtite_to_file()
 
 def wrtite_to_file():
     output_file = open('file', 'wb')
-    array_to_file = array('d', byte_table)
+    array_to_file = array('b', byte_table)
     array_to_file.tofile(output_file)
     output_file.close()
 
 def read_from_file():
     input_file = open('file', 'rb')
-    float_array = array('d')
-    float_array.fromstring(input_file.read())
+    file_array = array('b')
+    file_array.fromstring(input_file.read())
+    #print(file_array)
+    byte_table = file_array
+    print(byte_table)
+    input_file.close()
+   
+
+
+def get_data(op_id, page_id):
+    read_from_file()
+    for i in range(0, metadata_pos, 20):
+        if byte_table[i] == op_id and byte_table[i+1] == page_id:
+            print(struct.unpack(node_data_packet_builder.FORMAT, byte_table[i+20]))
+            break
+        
 
 def get_page_content(op_id, page_id):
     for page in page_list:
@@ -123,5 +145,11 @@ def listen_interface():
                 save_page(new_data[0],new_data[1],new_data[2],new_data[3])
 
 
+save_page(170,171,24,"ewre")
+save_page(177,178,24,'this')
+get_data(177,178)
+read_from_file()
+#save_page(179,180,24,'what')
+#read_from_file()
 
-listen_interface()
+#listen_interface()
--- packet_builders/node_broadcast.py
+++ packet_builders/node_broadcast.py
@@ -2,7 +2,6 @@ import struct
 
 # unsigned char, unsigned int.
 FORMAT = 'BI'
-sequence = 0
 
 # Create a packet acording to the parameters, is a struct with a specific format (FORMAT)
 def create(operation_id, size):
