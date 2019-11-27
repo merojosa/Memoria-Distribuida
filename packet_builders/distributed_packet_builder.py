5f910ba5bdee9398986d9aabd2c500af3b4ebce3
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-11-27 07:55:54 -0600

    Se evita basura al crear paquete

--- packet_builders/distributed_packet_builder.py
+++ packet_builders/distributed_packet_builder.py
@@ -3,7 +3,7 @@ from enum_operation_code import Operation_Code
 
 
 # ML-ID-NM
-INITIAL_FORMAT = 'BB'   # operation code, page id
+INITIAL_FORMAT = '=BB'   # operation code, page id
 
 
 def create_read_packet(operation_code, page_id):
@@ -13,10 +13,10 @@ def create_save_packet(operation_code, page_id, data_size, data):
     return struct.pack(get_save_format(data_size), operation_code, page_id, data_size, data)
 
 def create_ok_broadcast_packet():
-    return struct.pack('B', Operation_Code.OK.value)
+    return struct.pack('=B', Operation_Code.OK.value)
 
 def create_ok_local_packet(page_id):
-    return struct.pack("BB", Operation_Code.OK.value, page_id)
+    return struct.pack("=BB", Operation_Code.OK.value, page_id)
 
 def get_save_format(data_size):
     return INITIAL_FORMAT + 'I' + str(data_size) + 's'
