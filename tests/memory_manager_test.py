66a40895b95e065148d2f41d91a219f6756ec93e
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-11-28 02:55:33 -0600

    Auto stash before merge of "Jose-Andres" and "origin/Jose-Andres"

--- tests/memory_manager_test.py
+++ tests/memory_manager_test.py
@@ -9,7 +9,7 @@ def send_packet_interface():
     id1 = memory_manager.create_page()
     id2 = memory_manager.create_page()
 
-
+    
     memory_manager.write(id2, '%i{40}')
     memory_manager.write(id2, '%f{1.5}')
     memory_manager.write(id1, '%f{77}')
