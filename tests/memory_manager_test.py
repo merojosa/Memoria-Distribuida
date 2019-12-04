871fb8e04a1baa4f3d7afabab34f1b40a9b86743
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2019-12-04 10:08:01 -0600

    Test mejorado

--- tests/memory_manager_test.py
+++ tests/memory_manager_test.py
@@ -7,19 +7,26 @@ import memory_manager
 def send_packet_interface():
 
     id0 = memory_manager.create_page()
+    id1 = memory_manager.create_page()
 
 
     memory_manager.write(id0, '%i{40}')
     memory_manager.write(id0, '%f{1.5}')
+    memory_manager.write(id1, "%i{777}")
     
     memory_manager.save_page(id0)
+    memory_manager.save_page(id1)
 
-   # data = memory_manager.get_page_data(id0)
+    data0 = memory_manager.get_page_data(id0)
+    data1 = memory_manager.get_page_data(id1)
 
     print('send_packet_interface OK')
 
-    print("Datos obtenidos ", end="")
-   # print(data)
+    print("Datos obtenidos id0 ", end="")
+    print(data0)
+
+    print("Datos obtenidos id1 ", end="")
+    print(data1)
 
 
 send_packet_interface()
